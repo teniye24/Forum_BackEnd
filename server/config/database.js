16af4c99f53eccf0beea9b22bb5e9f84245ac5cb
teniye24 <teniye24@yahoo.com>
2023-11-03 18:02:28 -0400

    database deployed

--- server/config/database.js
+++ server/config/database.js
@@ -1,16 +1,14 @@
 require("dotenv").config();
 const mysql2 = require("mysql2");
-const pool = mysql2.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.MYSQL_DB,
-  connectionLimit: 10,
-});
+// const pool = mysql2.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.MYSQL_DB,
+//   connectionLimit: 10,
+// });
+const pool = mysql2.createConnection(process.env.DATABASE_URL);
 
-pool.getConnection(function (err, connection) {
-  console.log("Database connected");
-});
 
 let registration = `CREATE TABLE if not exists registration(
    user_id int auto_increment,
@@ -24,8 +22,8 @@ let profile = `CREATE TABLE if not exists profile(
    user_id int not null,
    first_name varchar(255) not null,
    last_name varchar(255) not null,
-   PRIMARY KEY (user_profile_id),
-   FOREIGN KEY (user_id) REFERENCES registration(user_id)
+   PRIMARY KEY (user_profile_id)
+
 )`;
 let question = `CREATE TABLE if not exists question(
    question_id int auto_increment,
@@ -34,17 +32,16 @@ let question = `CREATE TABLE if not exists question(
    question_code_block varchar(255),
    tags varchar(255),
    user_id int not null,
-   PRIMARY KEY (question_id),
-   FOREIGN KEY (user_id) REFERENCES registration(user_id)
+   PRIMARY KEY (question_id)
+  
 )`;
 let answer = `CREATE TABLE if not exists answer(
    answer_id int auto_increment,
    answer varchar(255) not null,
    user_id int not null,
    question_id int not null,
-   PRIMARY KEY (answer_id),
-   FOREIGN KEY (user_id) REFERENCES registration(user_id),
-   FOREIGN KEY (question_id) REFERENCES question(question_id)
+   PRIMARY KEY (answer_id)
+  
 )`;
 pool.query(registration, (err, results, fields) => {
   if (err) throw err;
